fc21b02e392c6a9e66c452dd8e1fd368ab5a7826
CristianCeccon <cristian_ceccon45@hotmail.com>
2022-09-26 20:32:23 -0300

    Caminho inicial

--- paginas/urls.py
+++ paginas/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import IndexView, SobreView, RoboView
 
 urlpatterns = [
-    path('inicio/sistema', IndexView.as_view(), name="index"),
+    path('', IndexView.as_view(), name="index"),
     path('sobre/', SobreView.as_view(), name="sobre"),
     path('robos/', RoboView.as_view(), name="robos"),
 ]
